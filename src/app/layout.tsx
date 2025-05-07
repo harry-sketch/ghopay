25b45554d3010899245cee9a25e0a2b131dc8378
Ramitphi <18ucs144@lnmiit.ac.in>
2025-05-07 23:50:10 +0530

    thirdweb init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { type Metadata } from "next";
 import { Geist } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
+import { ThirdwebProvider } from "thirdweb/react";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -22,7 +23,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geist.variable}`}>
       <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <TRPCReactProvider>
+          <ThirdwebProvider>{children}</ThirdwebProvider>
+        </TRPCReactProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+import { createThirdwebClient } from "thirdweb";
+import { ConnectButton } from "thirdweb/react";
 import { HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
+  const client = createThirdwebClient({
+    clientId: "11061b92f667a9945df17ac608a2e196",
+  });
+
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         GHO pAy
+        <ConnectButton client={client} />
       </main>
     </HydrateClient>
   );
