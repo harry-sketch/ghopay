be684da1d53b083a33603b0acf3937f8ab17ced4
harry-sketch <harshbisht90@gmail.com>
2025-05-07 23:02:36 +0530

    fix: build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div>GHO-Pay</div>
+        GHO pAy
       </main>
     </HydrateClient>
   );
