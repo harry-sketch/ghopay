31270aea1228554158cb5bcc2c93c245979e26bc
Ramitphi <18ucs144@lnmiit.ac.in>
2025-05-08 00:12:09 +0530

    add sign-in

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,10 +2,25 @@
 
 import Image from "next/image";
 import React from "react";
+import { createThirdwebClient } from "thirdweb";
+import { ConnectButton, ThirdwebProvider } from "thirdweb/react";
+import { inAppWallet } from "thirdweb/wallets";
 
 const page = () => {
+  const wallet = inAppWallet({
+    auth: {
+      mode: "popup", // options are "popup" | "redirect" | "window";
+      options: ["email", "google"], // ex: ["discord", "farcaster", "apple", "facebook", "google", "passkey"],
+      redirectUrl: "http://localhost:3000", // the URL to redirect to after authentication
+    },
+  });
+
+  const client = createThirdwebClient({
+    clientId: "11061b92f667a9945df17ac608a2e196",
+  });
+
   return (
-    <div className="flex h-72 w-full flex-col items-center justify-between">
+    <div className="flex max-h-screen w-full flex-col items-center gap-56 border border-black">
       <div className="flex items-center gap-3">
         <Image
           src="/assets/Logo.png"
@@ -19,13 +34,8 @@ const page = () => {
           GHOPay
         </div>
       </div>
-
-      <button
-        type="button"
-        className="w-full rounded-lg bg-[#005E0D] p-4 text-lg font-medium text-white md:w-96"
-      >
-        Connect
-      </button>
+      {/* <div className="w-full rounded-lg bg-[#005E0D] p-4 text-lg font-medium text-white md:w-96"> */}{" "}
+      <ConnectButton client={client} wallets={[wallet]} /> {/* </div> */}
     </div>
   );
 };
